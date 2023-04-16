d778c2373746977e03d665ec63c70599f83a8adc
Jaesuk <jaesuk95@outlook.com>
2023-04-17 02:05:37 +0900

    Spring Cloud Gateway RequestMapping 오타

--- cloud-service/src/main/java/com/example/cloudservice/config/GatewayConfig.java
+++ cloud-service/src/main/java/com/example/cloudservice/config/GatewayConfig.java
@@ -12,27 +12,14 @@ public class GatewayConfig {
         return builder.routes()
                 //first-service
                 .route(r -> r
-                        .host("")
                         .path("/first-service/**")
                         .uri("http://localhost:8081"))
                 //second-service
-                .route(r -> r.path("/second-service/**")
+                .route(r -> r
+                        .path("/second-service/**")
 //                        .filters(f -> f.addResponseHeader("second-request", "second-request-header")
 //                                .addResponseHeader("second-response", "second-response-header"))
                         .uri("http://localhost:8082"))
                 .build();
-
-        //     return builder.routes()
-        //      .route("r1", r -> r.host("**.baeldung.com")
-        //        .and()
-        //        .path("/baeldung")
-        //        .uri("http://baeldung.com"))
-        //      .route(r -> r.host("**.baeldung.com")
-        //        .and()
-        //        .path("/myOtherRouting")
-        //        .filters(f -> f.prefixPath("/myPrefix"))
-        //        .uri("http://othersite.com")
-        //        .id("myOtherID"))
-        //    .build();
     }
 }
--- second-service/src/main/java/com/example/secondservice/controller/ApiController.java
+++ second-service/src/main/java/com/example/secondservice/controller/ApiController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/first-service")
+@RequestMapping("/second-service")
 public class ApiController {
 
     @GetMapping("/welcome")
