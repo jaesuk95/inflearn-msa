f47c8a21367ade283c370591dc9fb69782731a5e
Jaesuk <jaesuk95@outlook.com>
2023-04-21 02:50:42 +0900

    spring security for Spring 3.0.5v

--- user-service/src/main/java/com/example/userservice/impl/UserServiceImpl.java
+++ user-service/src/main/java/com/example/userservice/impl/UserServiceImpl.java
@@ -8,6 +8,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.UUID;
@@ -18,6 +19,7 @@ import java.util.UUID;
 public class UserServiceImpl implements UserService {
 
     private final UserRepository userRepository;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
 
     @Override
     public UserDto createUser(UserDto userDto) {
@@ -26,7 +28,8 @@ public class UserServiceImpl implements UserService {
         ModelMapper mapper = new ModelMapper();
         mapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
         UserEntity userEntity = mapper.map(userDto, UserEntity.class);
-        userEntity.setEncryptedPwd("encrypted_password");
+        String bCryptPassword = bCryptPasswordEncoder.encode(userDto.getPwd());
+        userEntity.setEncryptedPwd(bCryptPassword);
         userRepository.save(userEntity);
         log.info("new user has been saved = {}", userEntity.getId());
 
